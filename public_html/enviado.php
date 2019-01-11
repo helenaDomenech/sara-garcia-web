de53d038e0586a66ef833175f3e90a296f6de6fb
Elena <e_cabello_servext_rsi@cajarural.com>
2019-01-11 12:50:50 +0100

    ajuste formulario

--- public_html/enviado.php
+++ public_html/enviado.php
@@ -5,22 +5,18 @@
     $mensaje = $_POST['mensaje'];
     $para = 'elena.cabello.alba@gmail.com';
     $titulo = 'ASUNTO DEL MENSAJE';
-    $header = 'From:'.$email;
+    $header = 'From: '.$email;
     $msjCorreo = "Nombre: $name\n E-Mail: $email\n Mensaje:\n $mensaje";
 
     if ($_POST['submit']) {
-        if (mail($para, $titulo, $msjCorreo, $header)) {
-            echo '<p>¡ Mensaje enviado !</p>';
+        if (mail ($para, $titulo, $msjCorreo, $header) ) {
+            echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+            window.location.href = 'http://www.saragarciaterapeuta.es/index.html';
+            </script>";
         } else {
             echo 'Falló el envio';
         }
     }
-
-    if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) {
-            echo '<p>Your message has been sent!</p>';
-        } else {
-            echo '<p>Something went wrong, go back and try again!</p>';
-        }
-    }
 ?>
+
